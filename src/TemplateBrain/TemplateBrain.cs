006d6d5795770df237d2fb357451fe3db709fc33
kkuntner <kkuntner@szintezis-net.hu>
2021-10-18 17:55:22 +0200

    Updated for 2021 competition

--- src/TemplateBrain/TemplateBrain.cs
+++ src/TemplateBrain/TemplateBrain.cs
@@ -1,5 +1,8 @@
-﻿using SnakeLib;
+﻿using System.Linq;
+using SnakeLib;
 using SnakeLib.Brains;
+using SnakeLib.Enums;
+using SnakeLib.PowerUps;
 
 namespace TemplateBrain
 {
@@ -9,7 +12,7 @@ namespace TemplateBrain
 
         public override string Author => "<noname>";
 
-        public override Direction GetNextMovement(bool grow)
+        public override Movement GetNextMovement(bool grow)
         {
             /* This is the method you need to implement. It is called by the framework before each step, and the Snake will move 
              * into the direction indicated by the returned Direction value.
@@ -24,6 +27,7 @@ namespace TemplateBrain
              *  this.Me.Tail                  - last point of the snake
              *  this.Me.Size                  - length
              *  this.Me.Score                 - score
+             *  this.Me.ThinkTime             - overall thinktime during a Round. Cannot exceed this.Field.ThinkTimeLimit.
              *
              *  this.Enemy                    - enemy snake, has same properties as Me
              *
@@ -41,16 +45,40 @@ namespace TemplateBrain
              *  this.Field.Apples[n].Location - location of the nth apple
              *  this.Field.AppleFrequency     - how often apples are displayed
              *
-             * It is a good idea to check every other properties to make the most of your snake brain!
+             *  this.Field.PowerUps           - collection of PowerUps currently in the game
+             *  this.Field.PowerUps.Any(x => x.Type == PowerType.NoWall)                                - check if there is NoWall powerup available on Field
+             *  this.PowerUps.Any(x => x.Type == PowerType.NoWall && x.UsageLeft > 0)                   - check if we have available NoWall powerup
+             *  this.Me.PowerUps.Any(x => x.Type == PowerType.NoWall && x.UsageLeft > 0)                - check if we have available NoWall powerup
+             *
+             *  new Movement { direction = Direction.Up }.ActivateDiagonalPowerUp(Direction.Right)      - example for Diagonal PowerUp usage. (passing in secondary direction)
+             *  new Movement { direction = Direction.Up }.ActivateReversePowerUp()                     - example for Reverse PowerUp usage.
+             *
+             *  powerups are setup in arena/Simple.arena
+             *
+             * It is a good idea to check other properties to make the most of your snake brain!
              * 
              * Also, look at the helper methods and events (AppleAdded, AppleGone, GetHeadForDirection)
+             * 
+             * 
+             * Publish your dll by creating a new Class Library into this solution based on TemplateBrain or simply override TemplateBrain.cs, 
+             * modify AssemblyName in Class Library properties and also modify the namespace of your brain class.
              */
 
             // Now we look around, and move to an empty location, if there is any...
-            return Field[GetHeadForDirection(Direction.Right)].IsEmpty ? Direction.Right
+            var direction = Field[GetHeadForDirection(Direction.Right)].IsEmpty ? Direction.Right
                 : (Field[GetHeadForDirection(Direction.Left)].IsEmpty ? Direction.Left
                 : (Field[GetHeadForDirection(Direction.Up)].IsEmpty ? Direction.Up
                 : Direction.Down));
+
+            var movement = new Movement { Direction = direction };
+
+            // If we have a diagonal powerup, and we are going up or down, then go diagonal!
+            // This makes no sense without checking if the target is an empty cell, but acts as an example
+            if (Me.PowerUps.Any(x => x.Type == PowerUpType.Diagonal && x.UsageLeft > 0) && 
+                (direction == Direction.Up || direction == Direction.Down))
+                movement.ActivateDiagonalPowerUp(Direction.Left);
+            
+            return movement;
         }
     }
 }
